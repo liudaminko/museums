eeb92f6c09b00b2dce85d4ce0b6595115497ef85
liudmyla minkovets <luda.min2003@gmail.com>
2025-05-21 13:10:12 +0300

    Initial Commit

---
+++ MuseumsMap/MuseumsMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumsMapApp.swift
+//  MuseumsMap
+//
+//  Created by Liudmyla Minkovets on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MuseumsMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuseumsMapTests/MuseumsMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumsMapTests.swift
+//  MuseumsMapTests
+//
+//  Created by Liudmyla Minkovets on 21.05.2025.
+//
+
+import Testing
+@testable import MuseumsMap
+
+struct MuseumsMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
